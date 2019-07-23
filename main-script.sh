ca561535ae2c16d09cbaee57e9e6fdf242ff474b
greatashkan <greatashkan@gmail.com>
2019-07-23 05:06:27 +0000

    Final Main Script

--- main-script.sh
+++ main-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-bash docker-bash.sh \
-	&& bash k8s-bash.sh \
-	&& bash prepare-node.sh \
-	&& bash k8s-init-bash.sh  \
-	&& bash ingress-deploy.sh  \
-	&& bash guestbook-bash.sh \
-	&& bash autoscaler
+bash scrips/docker-bash.sh \
+	&& bash scrips/k8s-bash.sh \
+	&& bash scrips/prepare-node.sh \
+	&& bash scrips/k8s-init-bash.sh  \
+	&& bash scrips/ingress-deploy.sh  \
+	&& bash scrips/guestbook-bash.sh \
+	&& bash scrips/autoscaler
